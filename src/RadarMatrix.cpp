49221028b2abe446c89e1b95da8d123d5438889b
Thorsten Riek <thorsten.riek@daimler.com>
2018-01-11 08:59:24 +0100

    verbose 4 tests

--- src/RadarMatrix.cpp
+++ src/RadarMatrix.cpp
@@ -39,8 +39,8 @@ char velout[2]; //Ausgabe für 2-Stellige Geschwindigkeit
 void setup()
 {
 
-    // Serial.begin(9600);
-    // Serial.print("Program started\n"); // die serial Print befehle und zu viele matrix.println
+    Serial.begin(9600);
+    Serial.print("Program started\n"); // die serial Print befehle und zu viele matrix.println
   // befehle führen zu Punkten auf dem Display
   //  delay(2000);// Give reader a chance to see the output.
 
@@ -48,11 +48,11 @@ void setup()
   pinMode(RADAR_OUT, OUTPUT);
   pinMode(TRIGGER_IN, INPUT);
 
-  matrix.begin(); //Dieser Befehl lässt den Radarsensor falsch messen. Die Werte werden zu hoch
+//  matrix.begin(); //Dieser Befehl lässt den Radarsensor falsch messen. Die Werte werden zu hoch
 
-  matrix.setTextWrap(false); // Don't wrap at end of line - will do ourselves
+//  matrix.setTextWrap(false); // Don't wrap at end of line - will do ourselves
   // fix the screen with black
-  matrix.fillScreen(matrix.Color333(0, 0, 0));
+//  matrix.fillScreen(matrix.Color333(0, 0, 0));
 
   // draw a circle in yellow
   matrix.fillCircle(16, 16, 14, matrix.Color333(3, 3, 0));
@@ -66,12 +66,22 @@ void setup()
 
 void loop()
 {
-  unsigned long T; // Periodendauer in us
+  unsigned long Th; // Periodendauer in µs  high
+  unsigned long Tl; // Periodendauer in µs low
+  double Tges; // Periodendauer in µs ges
   double f;        // Frequenz in MHz
   vmax = 0;        // Auf Integer gesetzt um Speicher und Rundung zu sparen
-
   // fill the screen with 'black'
-  matrix.fillScreen(matrix.Color333(0, 0, 0));
+//  matrix.fillScreen(matrix.Color333(0, 0, 0));
+    Serial.print("Messung started\n"); // die serial Print befehle und zu viele matrix.println
+  // Set up Timer1 for interrupt:
+  cli();
+  TCCR2A  = 0; 
+  TCCR2B  = 0; 
+//  ICR1    = 0;
+//  TIMSK1 = 0; // Enable Timer1 interrupt
+  sei();                // Enable global interrupts
+
 
   do
   {
@@ -79,13 +89,35 @@ void loop()
     analogWrite(RADAR_OUT, map(trigger, 0, 1023, 0, 255));
   } while (trigger > TRIGGER_LEVEL);
   analogWrite(RADAR_OUT, 0);
+  Serial.print("Trigger: "); 
+  Serial.print(trigger); 
+  Serial.print(" > TRIGGER_LEVEL = "); 
+  Serial.println(TRIGGER_LEVEL); 
   for (int i = 0; i < SCANS; i++) // 8 Messungen
   {
-    T = pulseIn(RADAR_IN, HIGH) + pulseIn(RADAR_IN, LOW);
-    f = 1 / (double)T;      // f=1/T
-    v = ((f * 1e6) / 44.0); // 24 GHz Radar
+
+
+    Serial.print("."); 
+    Th = pulseIn(RADAR_IN, HIGH) ;
+    Serial.print("."); 
+    Tl = pulseIn(RADAR_IN, LOW);
+    Serial.print("Th: "); 
+    Serial.print(Th);
+    Serial.print("µs  -  Tl: "); 
+    Serial.print(Tl);
+    Tges = (Th + Tl)/1e6;      
+    Serial.print("µs  -  Tges : "); 
+    Serial.print(Tges,10);
+    Serial.print("s  -  f: "); 
+    f = 1 / Tges;      // f=1/T
+    Serial.print(f,5);
+    Serial.print("Hz  -  v: "); 
+    v = (f / 44.0); // 24 GHz Radar ... siehe http://www.hjberndt.de/soft/radar/indexfall.html
+    Serial.print(v);
+    Serial.print("km/h  -  vmax: "); 
     vmax = max(v, vmax);    // Schleppzeiger
-                            //  Serial.println(v);
+    Serial.print(vmax);
+    Serial.print("km/h\n"); 
 
     // matrix.setCursor(0,0);    // start left, with 24 pixel shift to bottom
     // matrix.setTextSize(1);     // size 1 == 8 pixels high
@@ -94,18 +126,27 @@ void loop()
     // matrix.println(i);
   }
 
+  matrix.begin(); //Dieser Befehl lässt den Radarsensor falsch messen. Die Werte werden zu hoch
+
+    Serial.println("-----------------------"); 
+
+
   matrix.fillScreen(matrix.Color333(0, 0, 0));
   matrix.setCursor(0, 0); // start at top left
-  matrix.setTextSize(3);  // size 1 == 8 pixels high
+  matrix.setTextSize(1);  // size 1 == 8 pixels high
 
   sprintf(velout, "%d", vmax);
   matrix.setTextColor(matrix.Color333(1, 5, 1));
   matrix.print(velout);
+  matrix.setCursor(0, 8); // start at top left
+  matrix.print(Th);
+  matrix.setCursor(0, 16); // start left, with 16 pixel shift to bottom
+  matrix.print(Tl);
 
   matrix.setCursor(0, 24); // start left, with 24 pixel shift to bottom
   matrix.setTextSize(1);   // size 1 == 8 pixels high
   matrix.setTextColor(matrix.Color333(3, 3, 3));
   matrix.print(" km/h");
 
-  delay(2000); // 2 Sekunden
+  delay(12000); // 2 Sekunden
 }
